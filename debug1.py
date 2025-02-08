61b1284ffaea7804d6009209457f921497ab645c
daiccy21 <dannielsantosa@gmail.com>
2025-02-08 21:23:13 +0700

    Add files via upload

---
+++ debug1.py
@@ -0,0 +1,7 @@
+#contoh syntax error
+print("Kalkulator pengurangan sederhana Daniel")
+angka1 =int(input("masukan angka pertama;"))
+angka2 =int(input("masukan angka kedua:"))
+
+hasil = angka1-angka2
+print("hasilnya adalah ",hasil1)
