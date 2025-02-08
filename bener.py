bde2aa87ebe88ead565a2cb5363a8a2005456269
daiccy21 <dannielsantosa@gmail.com>
2025-02-08 21:22:50 +0700

    Add files via upload

---
+++ bener.py
@@ -0,0 +1,7 @@
+#contoh program yang benar
+print("Kalkulator pengurangan sederhana Daniel")
+angka1 =int(input("masukan angka pertama:"))
+angka2 =int(input("masukan angka kedua:"))
+
+hasil = angka1-angka2
+print("hasilnya adalah ",hasil)
