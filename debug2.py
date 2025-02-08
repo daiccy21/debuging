4515349c23898cf3e7804979a05b241b02aa6fe7
daiccy21 <dannielsantosa@gmail.com>
2025-02-08 21:23:37 +0700

    Add files via upload

---
+++ debug2.py
@@ -0,0 +1,7 @@
+#contoh runtime error karena kesalahan algoritma
+print("Kalkulator pengurangan sederhana Daniel")
+angka1 =int(input("masukan angka pertama:"))
+angka2 =int(input("masukan angka kedua:"))
+
+hasil = angka2-angka1
+print("hasilnya adalah ",hasil)
